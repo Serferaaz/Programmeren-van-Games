a8050547e803b00df4760357bb4d7c446d84bc07
Serferaaz <54798372+Serferaaz@users.noreply.github.com>
2021-05-08 12:49:41 +0200

    Had geen commentaar
    Ik was vergeten om commentaar toe te voegen aan het script. Het is nu in orde.

---
+++ BrickScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour {
+
+    // Dit heb ik gemaakt zodat je de bricks punten kan geven. In Unity zelf kan je aangeven hoeveel punten een brick is.
+    public int points;
+    
+}
